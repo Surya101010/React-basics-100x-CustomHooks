c2a70eae6092ae7a4ad1f383b7950323084109b2
surya <greatuchsurya@gmail.com>
2025-09-20 23:02:23 +0530

    before custom hooks with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from 'react'
 function App() {
-  const [Count,setCount] = useState(0);
-  function increaseCount(){
-    return setCount(c => c+1)
+  const [post, setPost]= useState({});
+  async function getposts(){
+    const response =await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const json = await response.json();
+    setPost(json);
   }
+  useEffect(()=>{
+    getposts();
+  },[])
   return (
     <>
-     hi
-     <button onClick={increaseCount}>Increaase {Count}</button>
-    </>
+    {post.title}
+     </>
   )
 }
 
